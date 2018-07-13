73893244e7275851eaa0a01aa5b1a5c7738bd541
Maitraye <maitraye.urmi@gmail.com>
2018-07-13 04:45:30 +0000

    add jumbotron and image styles

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ app.set("view engine", "ejs");
 var campgrounds = [
     {name:"Salmon Creek" , image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Campground.jpg"},
     {name:"Granite Hill" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeGqaQIuOnLN4ezWn4OaEYZOJxIFSg2vV7WGjFGAzRjMbOFsIdJw"},
+    {name:"Mountain Goat's Rest" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu2z1lsEUAANdmeT_yi3euMpgH-dwTS8VNr5veav7j_AeC6Byd"},
+    {name:"Salmon Creek" , image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Campground.jpg"},
+    {name:"Granite Hill" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeGqaQIuOnLN4ezWn4OaEYZOJxIFSg2vV7WGjFGAzRjMbOFsIdJw"},
+    {name:"Mountain Goat's Rest" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu2z1lsEUAANdmeT_yi3euMpgH-dwTS8VNr5veav7j_AeC6Byd"},
+    {name:"Salmon Creek" , image: "https://upload.wikimedia.org/wikipedia/commons/9/9b/Campground.jpg"},
+    {name:"Granite Hill" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeGqaQIuOnLN4ezWn4OaEYZOJxIFSg2vV7WGjFGAzRjMbOFsIdJw"},
     {name:"Mountain Goat's Rest" , image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRu2z1lsEUAANdmeT_yi3euMpgH-dwTS8VNr5veav7j_AeC6Byd"}
 ];
 
