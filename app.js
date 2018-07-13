f3e055033f79e02e61b38a1a0d40b6f5ff5a9a6c
Maitraye <maitraye.urmi@gmail.com>
2018-07-13 04:14:12 +0000

    post new campground

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
@@ -15,6 +17,20 @@ app.get("/", function(req, res){
 
 app.get("/campgrounds", function(req, res){
     res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
+});
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    //redirect to campgrounds page
+    res.redirect("/campgrounds");
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
